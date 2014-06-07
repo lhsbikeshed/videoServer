7ba4ce8031e6b5a3da4f91d55485f8a0102c79cd
Charles Yarnold <charlesyarnold@gmail.com>
2014-06-07 22:18:12 +0100

    initial version to test

--- oscServer/certOSC.py
+++ oscServer/certOSC.py
@@ -7,12 +7,14 @@ import subprocess
 import os
 import signal
 import time
+import datetime
 
 TCP_IP = '127.0.0.1'
 TCP_PORT = 9998
 TCP_PORT2 = 9999
 
 templatePath = "/home/solexious/Dropbox/shipshared/cert.svg"
+svgPath = "/home/solexious/Dropbox/shipshared/certs/"
 
 pilotName = ""
 tacticalName = ""
@@ -29,15 +31,16 @@ s = OSC.OSCServer(receive_address) # basic
 s.addDefaultHandlers()
 
 # define a message-handler function for the server to call.
-def death(addr, tags, stuff, source):
-    generateCerts()
+def death_handler(addr, tags, stuff, source):
+    for a in stuff:
+        generateCerts(a)
     
     
-def win(addr, tags, stuff, source):
-    generateCerts()
+def win_handler(addr, tags, stuff, source):
+    generateCerts("returned home triumphant!")
 
 
-def getNames(addr, tags, stuff, source):
+def getNames_handler(addr, tags, stuff, source):
     global pilotName
     global tacticalName
     global engineerName
@@ -45,6 +48,7 @@ def getNames(addr, tags, stuff, source):
     global gmName
     global teamName
     global groundCrewName
+    global locationName
     
     print "---"
     #   print "received new osc msg from %s" % OSC.getUrlStr(source)
@@ -53,25 +57,22 @@ def getNames(addr, tags, stuff, source):
     print "data %s" % stuff
     print "---"
     
-    for a in stuff:
-        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        s.connect((TCP_IP, TCP_PORT))
-        if a == 0:
-            s.send("image load 0 \n")
-            s.send("audio mixer source mute off 1 1 \n")
-        elif a == 1:
-            s.send("image load 0 /home/videoserver/videoServer/images/beginScreen.png \n")
-            s.send("audio mixer source mute on 1 1 \n")
-        elif a == 2:
-            s.send("image load 0 /home/videoserver/videoServer/images/technicalDifficulties.png \n")
-            s.send("audio mixer source mute on 1 1 \n")
-        elif a == 3:
-            s.send("image load 0 /home/videoserver/videoServer/images/endScreen.png \n")
-            s.send("audio mixer source mute on 1 1 \n")
-        s.close()
+    pilotName = stuff[0]
+    tacticalName = stuff[1]
+    engineerName = stuff[2]
+    captainName = stuff[3]
+    gmName = stuff[4]
+    teamName = stuff[5]
+    groundCrewName = stuff[6]
+    locationName = stuff[7]
+    
+def reset_handler(addr, tags, stuff, source):
+    global startEpoch
+    
+    startEpoch = int(time.time())
 
 
-def generateCerts():
+def generateCerts(endResult):
     global pilotName
     global tacticalName
     global engineerName
@@ -79,24 +80,76 @@ def generateCerts():
     global gmName
     global teamName
     global groundCrewName
+    global locationName
+    global startEpoch
     
+    today = datetime.date.today()
     
-    # Pilot cert
-    with open("out.txt", "wt") as fout:
-      with open(templatePath, "rt") as fin:
-          for line in fin:
-              fout.write(line.replace('%PLAYER_NAME%', pilotName))
-              fout.write(line.replace('%TEAM_NAME%', teamName))
-              fout.write(line.replace('%PLAYER_ROLE%', 'Pilot'))
-              fout.write(line.replace('%DATE%', 'Orange'))
-              fout.write(line.replace('%LOCATION%', locationName))
-              fout.write(line.replace('%LENGTH%', 'Orange'))
-              fout.write(line.replace('%STORY_LINE%', 'Orange'))
-              fout.write(line.replace('%END_RESULT%', 'Orange'))
-              fout.write(line.replace('%CAPTAIN_NAME%', 'Orange'))
-              fout.write(line.replace('%GROUND_CREW_NAMES%', 'Orange'))
+    if startEpoch and pilotName:
+        missionLength = int((int(time.time()) - startEpoch)/60)
+        
+        # Pilot cert
+        svgNamePilot = svgPath + pilotName + "-" + teamName + "-" + today.strftime('%d-%m-%y') + ".svg"
+        svgNamePilot.replace(" ", "")
+        with open(svgNamePilot, "wt") as fout:
+          with open(templatePath, "rt") as fin:
+              for line in fin:
+                  fout.write(line.replace('%PLAYER_NAME%', pilotName))
+                  fout.write(line.replace('%TEAM_NAME%', teamName))
+                  fout.write(line.replace('%PLAYER_ROLE%', 'Pilot'))
+                  fout.write(line.replace('%DATE%', today.strftime('%d/%m/%y')))
+                  fout.write(line.replace('%LOCATION%', locationName))
+                  fout.write(line.replace('%LENGTH%', missionLength))
+                  fout.write(line.replace('%STORY_LINE%', 'many hardships'))
+                  fout.write(line.replace('%END_RESULT%', endResult))
+                  fout.write(line.replace('%CAPTAIN_NAME%', captainName))
+                  fout.write(line.replace('%GROUND_CREW_NAMES%', groundCrewName))
+        
+        # Tactical cert
+        svgNameTactical = svgPath + tacticalName + "-" + teamName + "-" + today.strftime('%d-%m-%y') + ".svg"
+        svgNameTactical.replace(" ", "")
+        with open(svgNameTactical, "wt") as fout:
+          with open(templatePath, "rt") as fin:
+              for line in fin:
+                  fout.write(line.replace('%PLAYER_NAME%', pilotName))
+                  fout.write(line.replace('%TEAM_NAME%', teamName))
+                  fout.write(line.replace('%PLAYER_ROLE%', 'Tactical'))
+                  fout.write(line.replace('%DATE%', today.strftime('%d/%m/%y')))
+                  fout.write(line.replace('%LOCATION%', locationName))
+                  fout.write(line.replace('%LENGTH%', missionLength))
+                  fout.write(line.replace('%STORY_LINE%', 'many hardships'))
+                  fout.write(line.replace('%END_RESULT%', endResult))
+                  fout.write(line.replace('%CAPTAIN_NAME%', captainName))
+                  fout.write(line.replace('%GROUND_CREW_NAMES%', groundCrewName))
+        
+        # Engineer cert
+        svgNameEngineer = svgPath + engineerName + "-" + teamName + "-" + today.strftime('%d-%m-%y') + ".svg"
+        svgNameEngineer.replace(" ", "")
+        with open(svgNameEngineer, "wt") as fout:
+          with open(templatePath, "rt") as fin:
+              for line in fin:
+                  fout.write(line.replace('%PLAYER_NAME%', pilotName))
+                  fout.write(line.replace('%TEAM_NAME%', teamName))
+                  fout.write(line.replace('%PLAYER_ROLE%', 'Engineer'))
+                  fout.write(line.replace('%DATE%', today.strftime('%d/%m/%y')))
+                  fout.write(line.replace('%LOCATION%', locationName))
+                  fout.write(line.replace('%LENGTH%', missionLength))
+                  fout.write(line.replace('%STORY_LINE%', 'many hardships'))
+                  fout.write(line.replace('%END_RESULT%', endResult))
+                  fout.write(line.replace('%CAPTAIN_NAME%', captainName))
+                  fout.write(line.replace('%GROUND_CREW_NAMES%', groundCrewName))
+                  
+    pilotName = ""
+    tacticalName = ""
+    engineerName = ""
+    captainName = ""
+    gmName = ""
+    teamName = ""
+    groundCrewName = ""
+    startEpoch = 0
     
-s.addMsgHandler("/screen/focus", screenFocus_handler) # adding our function
+s.addMsgHandler("/scene/youaredead", death_handler) # adding our function
+s.addMsgHandler("/game/gameWin", win_handler) # adding our function
 
 
 # just checking which handlers we have added
